fa68ac635acf2135a24de5f3ff91400ad600c61e
JMJ Riyana <jeemariyana@gmail.com>
2020-10-16 00:29:46 +0530

    adddependencies

--- demo/src/main/java/com/example/demo/UserController.java
+++ demo/src/main/java/com/example/demo/UserController.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
