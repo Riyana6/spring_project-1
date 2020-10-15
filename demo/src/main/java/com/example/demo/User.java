ffe1d3eec1eb4f0e746f981b340ffdde6e1562cb
JMJ Riyana <jeemariyana@gmail.com>
2020-10-15 23:25:13 +0530

    initi

---
+++ demo/src/main/java/com/example/demo/User.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    private String id;
+    private String Name;
+    private String Email;
+    private String Password;
+
+}
