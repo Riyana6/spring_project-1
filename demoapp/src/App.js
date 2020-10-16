31cd96bcf7d34210568bb1db53bcb3f458cfa8e2
JMJ Riyana <jeemariyana@gmail.com>
2020-10-16 19:57:37 +0530

    setdemoapp

--- demoapp/src/App.js
+++ demoapp/src/App.js
@@ -3,7 +3,30 @@ import React from 'react';
 class App extends React.Component {
   render(){
     return (
-      <div className="App">
+      <div className="Container">
+        <div className="row">
+          <div className="col s6">
+            <form>
+              <div class="input-field col s12">
+                <i class="material-icons prefix">person</i>
+                <input type="text" id="autocomplete-input" class="autocomplete"/>
+                <label for="autocomplete-input">Enter name</label>
+              </div>
+              <div class="input-field col s12">
+                <i class="material-icons prefix">mail</i>
+                <input type="email" id="autocomplete-input" class="autocomplete"/>
+                <label for="autocomplete-input">Enter email</label>
+              </div> 
+              <div class="input-field col s12">
+                <i class="material-icons prefix">vpn_key</i>
+                <input type="password" id="autocomplete-input" class="autocomplete"/>
+                <label for="autocomplete-input">Enter password</label>
+              </div> 
+            </form>
+          </div>
+          <div className="col s6">
+          </div>
+        </div>
       </div>
     );
   }
